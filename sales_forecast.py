b98bdb2b0d39db78618b37ad69ae70de8fe2b0a2
agapekumpati <agapekumpati@gmail.com>
2026-06-17 17:09:54 +0530

    Add files via upload

--- sales_forecast.py
+++ sales_forecast.py
@@ -1,2 +1,30 @@
-print("Future Interns ML Task 1")
-print("Sales Forecasting Project")
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load dataset
+df = pd.read_csv("Sample - Superstore.csv", encoding='latin1')
+
+# Convert Order Date to datetime
+df['Order Date'] = pd.to_datetime(df['Order Date'])
+
+# Sort by date
+df = df.sort_values('Order Date')
+
+# Group sales by date
+sales_data = df.groupby('Order Date')['Sales'].sum()
+
+# Plot sales trend
+plt.figure(figsize=(12,6))
+plt.plot(sales_data)
+plt.title("Sales Trend Over Time")
+plt.xlabel("Date")
+plt.ylabel("Sales")
+plt.grid()
+
+# Save graph
+plt.savefig("forecast_graph.png")
+
+# Show graph
+plt.show()
+
+print("Graph created successfully!")
